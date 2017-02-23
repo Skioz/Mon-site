e900f4cbaaef45c41ae2e407e2cbc346a0e00fe8
Skioz <syfos94@gmail.com>
2017-02-23 10:49:36 +0100

    init

---
+++ formulaire.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php include "navbar.php"  ?>
+
+<p>
+    
+    Veuillez taper votre prénom :
+</p>
+
+<form action="cible.php" method="post">
+<p>
+    <input type="text" name="prenom" />
+    <input type="submit" value="Valider" />
+</p>
+</form>
+
+</body>
+</html>
