6b8676ca4ded9eafc372a1f91cca6e33e41f86aa
Skioz <syfos94@gmail.com>
2017-03-02 10:40:10 +0100

    MAJ code

--- navbar.php
+++ navbar.php
@@ -16,7 +16,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">S'inscrire<span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="register.php">S'inscrire<span class="sr-only">(current)</span></a></li>
         <li><a href="connect.php">Connectez-Vous</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gestion du compte <span class="caret"></span></a>
